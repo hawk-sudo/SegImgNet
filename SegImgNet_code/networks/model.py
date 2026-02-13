d93328902d2c2c896b400826f0458b2202c44f70
Yixin <zxcxzcz@qq.com>
2026-02-13 23:52:00 +0000

    update code

--- SegImgNet_code/networks/model.py
+++ SegImgNet_code/networks/model.py
@@ -2,9 +2,11 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from networks.U_net import U_Net，U_Net_mid_output
+from networks.U_net import U_Net,U_Net_mid_output
 import timm
-from networks.ResUnet import ResNet34_UNet
+
+
+def get_Model(model_name):
     if model_name == 'Unet':
         model = U_Net(img_ch=3,output_ch=2)
     if model_name == 'U_Net_mid_output':
